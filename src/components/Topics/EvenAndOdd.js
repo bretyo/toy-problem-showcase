236aa5a1b84e88caf0e82e0b232e6bfc3a68ae3c
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-20 15:50:44 -0600

    Saving progress. on step 3 now

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EvenAndOdd extends Component{
+
+    render(){
+
+        return(
+            <p>Even And Odd</p>
+        )
+    }
+}
+
+export default EvenAndOdd;
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FilterObject extends Component{
+
+    render(){
+
+        return(
+            <p>Filter Object</p>
+        )
+    }
+}
+
+export default FilterObject;
