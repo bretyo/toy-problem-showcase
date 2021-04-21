5f1d220b59230c933e2e389df90356e4b997ecb0
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-20 19:22:26 -0600

    Finished Palindromes, and it works with spaces and capital letters!

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
 
 class Palindrome extends Component{
+    constructor(){
+        super();
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+
+        this.checkPalindrome = this.checkPalindrome.bind(this);
+    }
+
+    // THIS WORKS WITH SPACES AND CAPITAL LETTERS!
+    checkPalindrome(){
+        let s = this.state.userInput.split(' ').join('');
+        const arr = [...s.slice(0,s.length/2)].reverse().join('');
+        let word = '';
+        if(s.slice(Math.ceil(s.length/2)).toUpperCase().trim() === arr.toUpperCase()){
+            word = 'PALINDROME';
+        }
+        else{
+            word = 'NOT PALINDROME'
+        }
+        this.setState({palindrome: word});
+    }
 
     render(){
 
         return(
-            <p>Palindrome</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Palindrome</h4>
+                <input onChange={(e)=>{this.setState({userInput: e.target.value})}} className="inputLine"/>
+                <button onClick={this.checkPalindrome} className="confirmationButton">Check</button>
+                <span className="resultsBox">{this.state.palindrome}</span>
+            </div>
         )
     }
 }
