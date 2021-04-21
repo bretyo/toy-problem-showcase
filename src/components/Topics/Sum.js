c5990664096e61e5b341af9b471782924aeb231d
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-20 19:41:54 -0600

    Finished Sum! And it checks for non-numbers!

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 
 class Sum extends Component{
+    constructor(){
+        super();
+        this.state = {
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+        this.add = this.add.bind(this);
+    }
+    // THIS ACCOUNTS FOR IF THE INPUT VALUES AREN'T NUMBERS
+    add(){
+        Number.parseFloat(this.state.number1) && Number.parseFloat(this.state.number2) ? 
+            this.setState({ sum: Number.parseFloat(this.state.number1) + Number.parseFloat(this.state.number2) })
+            : this.setState({ sum: 'Both values need to be a number!'})
+    }
 
     render(){
 
         return(
-            <p>Sum</p>
+            <div className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <input onChange={(e)=>{this.setState({number1: e.target.value})}} className="inputLine"/>
+                <input onChange={(e)=>{this.setState({number2: e.target.value})}} className="inputLine"/>
+                <button onClick={this.add} className="confirmationButton">Add!</button>
+                <span className="resultsBox">{this.state.sum}</span>
+            </div>
         )
     }
 }
